1e7b2ef62d4500e772eded27a0f0304fbfe6ae2c
VamsiKrishna1805 <57950146+VamsiKrishna1805@users.noreply.github.com>
2021-03-23 23:22:18 +0530

    DMD-264: issue with UITableView resolved (#32)

--- KountDataCollectorUnivesalLibrary/TouchControls/UIControl+CustomControl.m
+++ KountDataCollectorUnivesalLibrary/TouchControls/UIControl+CustomControl.m
@@ -120,19 +120,3 @@
 }
 
 @end
-
-@implementation UITableView (CustomTableView)
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self.nextResponder touchesBegan:touches withEvent:event];
-}
-
-- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self.nextResponder touchesEnded:touches withEvent:event];
-}
-
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self.nextResponder touchesMoved:touches withEvent:event];
-}
-
-@end
