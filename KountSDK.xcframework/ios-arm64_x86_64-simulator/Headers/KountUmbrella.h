f24dc63b5be5ff26845d4f19da886b004bd2156c
Juan Pablo Leiva <juan.leiva@equifax.com>
2026-09-25 14:20:50 +0000

    chore(release): prepare KountSDK v6.0.0 for release (XCFramework and Universal Library updated)

---
+++ KountSDK.xcframework/ios-arm64_x86_64-simulator/Headers/KountUmbrella.h
@@ -0,0 +1,15 @@
+//
+//  KountUmbrellaHeader.h
+//  KountDataCollector
+//
+//  Created by Felipe Plaza on 08-07-24.
+//  Copyright © 2024 Kount Inc. All rights reserved.
+//
+
+#import <KountAnalyticsViewController.h>
+#import <KDataCollector.h>
+
+#import <KDataCollector_Internal.h>
+#import <KEnvironmentURLProvider_Internal.h>
+#import <KCollectorTaskBase.h>
+
---
+++ KountSDKUniversalLibrary/KountSDK.h
@@ -0,0 +1,12 @@
+//
+//  KountSDK.h
+//  KountDataCollector
+//
+//  Public umbrella header for the KountSDK module.
+//  Clients should use: @import KountSDK; or #import <KountSDK/KountSDK.h>
+//
+//  Copyright © 2024 Kount Inc. All rights reserved.
+//
+
+#import <KDataCollector.h>
+#import <KountAnalyticsViewController.h>
