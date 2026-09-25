a8b79221a3d0a9663f3fe657e1df0118145464cc
Felipe Plaza <felipe.plaza@equifax.com>
2026-09-25 14:20:39 -0300

    chore: Prepare examples for Kount SDK v6.0.0 release

---
+++ CheckoutExampleObjC/CheckoutExampleObjC/CheckoutViewController.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+@import KountSDK;
+@class CheckoutViewController;
+
+@protocol CheckoutViewControllerDelegate <NSObject>
+- (void)checkoutViewControllerDidFinish:(CheckoutViewController *)controller;
+@end
+
+@interface CheckoutViewController : KountAnalyticsViewController
+@property (nonatomic, weak) id <CheckoutViewControllerDelegate> delegate;
+@property (nonatomic) IBOutlet UITextView *textView;
+- (IBAction)done:(id)sender;
+@end
